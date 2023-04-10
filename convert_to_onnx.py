4bad9a70e920beefddb28096fc94816e2fa88b02
anirhc <anirhc@gmail.com>
2023-04-10 23:43:11 +0800

    Fixes to convert_to_onnx.py

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,10 +1,10 @@
 import torch
-from train import DeepNeuralNet
+import model as m
 import numpy as np
 
 
 def main():
-  pytorch_model = DeepNeuralNet(n_x = 164, n_h = [64, 32, 8], n_y = 1)
+  pytorch_model = m.DeepNeuralNet(n_x = 164, n_h = [64, 32, 8], n_y = 1)
   pytorch_model.load_state_dict(torch.load('pytorch_model.pt'))
   pytorch_model.eval()
   dummy_input = torch.tensor(np.random.randint(2, size=164), dtype=torch.float64)
